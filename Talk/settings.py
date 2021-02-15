94fde3de2e857887039cddc75b81cbd3c6c542b8
Meghna918 <39947684+Meghna918@users.noreply.github.com>
2021-02-15 17:06:05 +0530

    Update settings.py

--- Talk/settings.py
+++ Talk/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&qokltnc1d4i^we_ukzhy#v#+y2syr!xscsw_#p3dzi2%2cl)^'
-
+SECRET_KEY = z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -81,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Talk',
-        'USER': 'postgres',
-        'PASSWORD': 'prettyboy@828116',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
